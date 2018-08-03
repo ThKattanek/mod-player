7e6a761fc04988ff44fb5f3b2e91b054c0408eb2
Kattanek <kattanek@problech-gmbh.local>
2018-08-03 11:16:20 +0200

    mod samples werden eingelesen

--- mod_class.cpp
+++ mod_class.cpp
@@ -40,9 +40,9 @@ void MODClass::MODRead(const char *filename)
     else if(!strcmp((const char*)mod_type, "8CHN"))
         mod_type_id = _8CHN;
     else if(!strcmp((const char*)mod_type, "FLT4"))
-        mod_type_id = _FLT4;
+        mod_type_id = _4FLT;
     else if(!strcmp((const char*)mod_type, "FLT8"))
-        mod_type_id = _FLT8;
+        mod_type_id = _8FLT;
     else
     {
         mod_type_id = _UNKNOWN;
@@ -50,8 +50,66 @@ void MODClass::MODRead(const char *filename)
         file.close();
         return;
     }
-
     cout << "Modtype: " << mod_type << endl;
+
+    // SAMPLES
+    if(mod_type_id == _UNKNOWN)
+        mod_sample_count = 15;
+    else mod_sample_count = 31;
+
+    cout << endl;
+    cout << mod_sample_count << " samples " << "used in this MOD" << endl;
+
+    for(int i=0; i<mod_sample_count; i++)
+    {
+        // Sample Name
+        file.read(mod_samples[i].name,22);
+        mod_samples[i].name[22] = 0;
+
+        cout << "Nr.: " << i+1 << " - " << mod_samples[i].name << endl;
+
+        // Sample length
+        file.read((char*)&mod_samples[i].length,2);
+        unsigned char *length = (unsigned char*)&mod_samples[i].length;
+        unsigned char tmp = length[0];
+        length[0] = length[1];
+        length[1] = tmp;
+        mod_samples[i].length *=2;
+
+        cout << "\tLength: " << mod_samples[i].length << endl;
+
+        // Finetune
+        unsigned char finetune;
+        file.read((char*)&finetune,1);
+        mod_samples[i].finetune = FINETUNETBL[finetune];
+
+        cout << "\tFinetune: " << (int)mod_samples[i].finetune << endl;
+
+        // Volume
+        file.read((char*)&mod_samples[i].volume,1);
+        cout << "\tVolume: " << (int)mod_samples[i].volume << endl;
+
+        // Loop start
+        file.read((char*)&mod_samples[i].loop_start,2);
+        unsigned char *loop_start = (unsigned char*)&mod_samples[i].loop_start;
+        tmp = loop_start[0];
+        loop_start[0] = loop_start[1];
+        loop_start[1] = tmp;
+        mod_samples[i].loop_start *=2;
+
+        cout << "\tLoopStart: " << mod_samples[i].loop_start << endl;
+
+        // Loop length
+        file.read((char*)&mod_samples[i].loop_length,2);
+        unsigned char *loop_length = (unsigned char*)&mod_samples[i].loop_length;
+        tmp = loop_length[0];
+        loop_length[0] = loop_length[1];
+        loop_length[1] = tmp;
+        mod_samples[i].loop_length *=2;
+
+        cout << "\tLoopLength: " << mod_samples[i].loop_length << endl;
+
+    }
 
     file.close();
 }
--- mod_class.h
+++ mod_class.h
@@ -7,10 +7,23 @@
 
 using namespace std;
 
-enum MOD_TYPE_ID {_MK, _4CHN, _6CHN, _8CHN, _FLT4, _FLT8, _UNKNOWN};
+enum MOD_TYPE_ID {_MK, _4CHN, _6CHN, _8CHN, _4FLT, _8FLT, _UNKNOWN};
+
+static const char FINETUNETBL[] = {0, 1, 2, 3, 4, 5, 6, 7, -8, -7, -6, -5, -4, -3, -2, -1};
+
+struct SAMPLE
+{
+    char name[23];
+    unsigned short length;
+    char finetune;
+    unsigned char volume;
+    unsigned short loop_start;
+    unsigned short loop_length;
+};
 
 class MODClass
 {
+
 public:
     MODClass(const char *filename);
 private:
@@ -22,7 +35,9 @@ private:
     // MOD
     char mod_name[21];
     unsigned char mod_type[5];
-    int mod_type_id;
+    unsigned char mod_type_id;
+    int mod_sample_count;
+    SAMPLE mod_samples[31];
 };
 
 #endif // MODCLASS_H
